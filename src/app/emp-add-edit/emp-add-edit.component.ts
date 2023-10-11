b4abca94de99ccf1bc4ab81cf987e0516533ca9a
Fabio Trujillo <f.trujillo2@dxc.com>
2023-10-11 11:46:49 -0600

    Agreagado metodo de cerrar dialogo

--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms'
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-emp-add-edit',
   templateUrl: './emp-add-edit.component.html',
   styleUrls: ['./emp-add-edit.component.css']
 })
-export class EmpAddEditComponent {
+export class EmpAddEditComponent implements OnInit{
+
+  constructor(private dialogRef: MatDialog) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  login(form:NgForm) {
+
+    const user = form.value.user
+    const psw = form.value.password
+
+    if(user != "admin" || psw != "root") {
+      console.log("Usuario o pasword incorrectos!!")
+    }
+    else {
+      console.log("Usuario admin")
+    }
+  }
+
+  close () {
+     let dialogRef = this.dialogRef;
+     dialogRef.closeAll();
+   } 
 
 }
 
