ec90195d92fb6712aaf84959ae111ca711fede97
Fabio Trujillo <f.trujillo2@dxc.com>
2023-10-11 11:15:28 -0600

    Primer avance: Toolbar e inicio login

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Route } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NgModel } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proyectoFinal';
+
+  constructor(private _dialog: MatDialog) {}
+
+openAddEditEmpForm() {
+  this._dialog.open(EmpAddEditComponent);
+}
+
+}
+
+
+ /*
+const appRoutes:Route=[
+  {path:'login', component:LoginComponent}
+];
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ]
+});
+*/
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component'
+import {MatDialogModule} from '@angular/material/dialog'
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    EmpAddEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
